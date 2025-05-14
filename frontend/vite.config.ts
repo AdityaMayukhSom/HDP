16b3217eb72cc6c39a7d2c1bc7f0ea83aaf7b219
Aditya Mayukh Som <adi.kg2@gmail.com>
2025-05-14 07:19:32 +0000

    added react for frontend

---
+++ frontend/vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: "/",
+  server: {
+    host: "0.0.0.0",
+    allowedHosts: ["host.docker.internal", "prime-ubuntu"],
+  },
+  build: {
+    outDir: "./dist",
+    emptyOutDir: true,
+  },
+});
--- main.py
+++ main.py
@@ -1,27 +1,42 @@
+import ssl
 import sys
 
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.gzip import GZipMiddleware
 from fastapi.staticfiles import StaticFiles
 
 from src.api import api_router
 from src.web import web_router
 
 app = FastAPI()
-app.mount("/public", StaticFiles(directory="public"), name="public")
+app.add_middleware(GZipMiddleware, minimum_size=1000, compresslevel=5)
+
+app.mount(
+    "/public",
+    StaticFiles(directory="public", check_dir=False, follow_symlink=True),
+    name="public",
+)
+
 app.include_router(api_router)
 app.include_router(web_router)
 
 
 def main(argv: list[str]):
+    # ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+    # ssl_context.load_cert_chain(
+    #     certfile="/etc/ssl/certs/apache-selfsigned.crt",
+    #     keyfile="/etc/ssl/private/apache-selfsigned.key",
+    # )
+
     uvicorn.run(
         "main:app",
-        host="127.0.0.1",
+        host="0.0.0.0",
         port=5000,
-        log_level="info",
+        log_level="debug",
         reload=True,
-        # ssl_keyfile="./certs-localhost/key.pem",
-        # ssl_certfile="./certs-localhost/cert.pem",
+        use_colors=True,
+        # ssl=ssl_context,
     )
 
 
